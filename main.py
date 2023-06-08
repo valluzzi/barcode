ec441fe2c19db5e0cf905a9bd7158450b3bf341f
valluzzi <valluzzi@gmail.com>
2023-06-08 19:09:14 +0200

    some fix

--- main.py
+++ main.py
@@ -4,39 +4,69 @@ import cv2
 import pytesseract
 
 def deskew_image_cv(file_path):
-    # Load the image
-    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-    #ret, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
-    # Invert the image
-    img = cv2.bitwise_not(img)
-
-    # Detect lines in the image using the Hough transform
-    coords = np.column_stack(np.where(img > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    print(angle)
-    # The `cv2.minAreaRect` function returns values in the range [-90, 0); as the rectangle rotates clockwise the angle tends to 0 
-    # so it's necessary to add 90 degrees to the angle if it's found in the range [-90,-45)
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-
-    # Rotate the image to deskew it
-    (h, w) = img.shape[:2]
-    center = (w // 2, h // 2)
+    # Load image
+    img = cv2.imread(file_path, 0)
     
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    deskewed_img = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    # Thresholding the image
+    (thresh, img_bin) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    
+    # Invert the image
+    img_bin = 255-img_bin
 
-    return deskewed_img
+    # Define a rectangular structuring element
+    sz = np.array(img.shape) // 40
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, tuple(sz))
+
+    # Apply dilation on the threshold image
+    dilation = cv2.dilate(img_bin, rect_kernel, iterations = 1)
+
+    # Find contours
+    contours, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Loop over each contour
+    angles = []
+    for cnt in contours:
+        # Get the minimum area rectangle
+        rect = cv2.minAreaRect(cnt)
+        angles.append(rect[-1])
+
+    # Compute the median angle and rotate
+    median_angle = np.median(angles)
+    if median_angle > 45:
+        median_angle -= 90
+    img_rotated = rotate_image(img, -median_angle)
+
+    return img_rotated
+
+def rotate_image(mat, angle):
+    # angle in degrees
+    height, width = mat.shape[:2]
+    image_center = (width / 2, height / 2)
+
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+    abs_cos = abs(rotation_mat[0,0])
+    abs_sin = abs(rotation_mat[0,1])
+
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
 
 def read_barcode(file_path):
     # Open the image file
     
-    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-    # Apply binary thresholding
-    threshold = 100
-    _, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+    # img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    # # Apply binary thresholding
+    # threshold = 100
+    # _, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+    
+    img = deskew_image_cv(file_path)
+
     
     # Use pyzbar to find and decode the barcode in the image
     barcodes = decode(img)
@@ -44,9 +74,16 @@ def read_barcode(file_path):
     text = pytesseract.image_to_string(img).strip()
     print(f"[INFO] Text: <{text}>")
     
+
+    print(f"[INFO] Found {len(barcodes)} barcode(s)")
     for barcode in barcodes:
         barcode_data = barcode.data.decode("utf-8")
         print(f"[INFO] Barcode {barcode.type}: {barcode_data}")
+    print("===========================")
+    cv2.imshow(file_path,img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 # Usage
 # list all png and jpg files in the current directory
